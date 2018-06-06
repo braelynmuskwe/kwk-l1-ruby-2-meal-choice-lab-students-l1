b353a81942da6481995ba8b8818246d09a5638e4
braelynmuskwe <braemira1@icloud.com>
2018-06-06 15:57:11 +0000

    Done.

---
+++ boolean.rb
@@ -0,0 +1,4 @@
+#booleans are values that can either be true or false
+# 1 equal sign is a variable and 2 equal signs test the validity of an equation
+# && == and
+# || == or
--- meal_choice.rb
+++ meal_choice.rb
@@ -1,10 +1,23 @@
 # Here's an example of a 'snacks' method that returns the meal choice passed in to it and defaults to "cheetos" if nothing is passed in.
-def snacks(food="Cheetos")
-  "Any time, is the right time for #{food}!" 
+def meal_choice(breakfast)
+
+  puts "morning is the best time for #{breakfast}"
+   
 end
-
+meal_choice("scrambled eggs")
 # Define breakfast, lunch and dinner methods that return the meal choice passed into them with a default to your favorite.
+def meal_choice(lunch)
+  
+  puts "lunch is the best time for #{lunch}"
+  
+end
+meal_choice("pizza")
 
+def meal_choice(dinner)
+  
+  puts "dinner is the best time for #{dinner}"
+end
+meal_choice(dinner)
 
 
 # Call the methods with puts and your own arguments here. Like this:
